93b83b77aec84d21072a05546f202b8f92ff4e8d
OlaGreat <oladipupoolamilekan2@gmail.com>
2025-09-11 10:50:27 +0100

    update

--- src/hooks/events.tsx
+++ src/hooks/events.tsx
@@ -1,64 +1,8 @@
-// import { parseAbiItem } from 'viem'
-// import { publicClient } from '../config/publicClient'
-// import { useEffect, useState } from 'react'
-// import { useAccount } from 'wagmi'
-
-// const STAKING_CONTRACT_ADDRESS = import.meta.env.VITE_STAKING_CONTRACT
-
-// export function useStakingEvents() {
-//   const [stakeEvents, setStakeEvents] = useState<any[]>([])
-//   const [withdrawEvents, setWithdrawEvents] = useState<any[]>([])
-//   const [loading, setLoading] = useState(false)
-//   const [error, setError] = useState<string | null>(null)
-//   const { address } = useAccount() 
-//   useEffect(() => {
-//     if (!address) return
-
-//     setLoading(true)
-//     setError(null)
-
-//     const unwatchStake = publicClient.watchEvent({
-//       address: STAKING_CONTRACT_ADDRESS,
-//       event: parseAbiItem(
-//         'event Staked(address user,uint256 amount,uint256 timestamp,uint256 newTotalStaked,uint256 currentRewardRate)'
-//       ),
-//       onLogs: logs => {
-//         const userLogs = logs.filter(log => log.args.user?.toLowerCase() === address.toLowerCase())
-//         if (userLogs.length > 0) {
-//           setStakeEvents(prev => [...prev, ...userLogs])
-//         }
-//       }
-//     })
-
-//     const unwatchWithdraw = publicClient.watchEvent({
-//       address: STAKING_CONTRACT_ADDRESS,
-//       event: parseAbiItem(
-//         'event Withdrawn(address user,uint256 amount,uint256 timestamp,uint256 newTotalStaked,uint256 currentRewardRate,uint256 rewardsAccrued)'
-//       ),
-//       onLogs: logs => {
-//         const userLogs = logs.filter(log => log.args.user?.toLowerCase() === address.toLowerCase())
-//         if (userLogs.length > 0) {
-//           setWithdrawEvents(prev => [...prev, ...userLogs])
-//         }
-//       }
-//     })
-
-//     setLoading(false)
-
-//     return () => {
-//       unwatchStake()
-//       unwatchWithdraw()
-//     }
-//   }, [address])
-
-//   return { stakeEvents, withdrawEvents, loading, error }
-// }
 
 import { parseAbiItem } from 'viem'
 import { publicClient } from '../config/publicClient'
 import { useEffect, useState } from 'react'
 import { useAccount } from 'wagmi'
-import { STAKING_ABI } from '../abi/staking'
 
 const STAKING_CONTRACT_ADDRESS = import.meta.env
   .VITE_STAKING_CONTRACT as `0x${string}`
